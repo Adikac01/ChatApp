8c501ac53a421aec625bbba6c14f904bc5840781
Adrian Kacperski <adikac919@o2.pl>
2023-04-06 15:12:52 +0200

    first commit

---
+++ ChatNetworking/include/ChatNetworking/Base.h
@@ -0,0 +1,5 @@
+//
+// Created by 434578 on 06.04.2023.
+//
+
+#pragma once
---
+++ ChatNetworking/src/Base.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 434578 on 06.04.2023.
+//
+
+#include <ChatNetworking/Base.h>
+
+enum class MyEnum{
+    One,
+    Two,
+    Three
+};
